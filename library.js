2ec73e5e9ca54b166251a0d2bc26b504ecb5a9bc
Sam <dacosta.peon@gmail.com>
2024-05-29 18:42:16 +0200

    the remove buttons work, the read toggles not

--- library.js
+++ library.js
@@ -5,13 +5,13 @@ const bookForm = document.getElementById("bookForm");
 const closeBtn = document.getElementById("closeBtn");
 const showBtn = document.querySelector(".add button");
 
+
 //var for form
 
 let title = document.getElementById("title");
     let author = document.getElementById("author");
     let pages = document.getElementById("pages");
-    let chread = document.getElementById("read");
-
+    let read = document.getElementById("read");
 
 
 //New Book button opens dialogue box and closeBtn closes it
@@ -40,17 +40,6 @@ bookForm.addEventListener("submit", (e) => {
         alert("Please check if you forgot something");
     } else {
 
-        if (chread.checked == true) {
-            
-            read = "I've already read it";
-            chread.checked = false;
-
-        } else {
-
-            read = "not yet";
-
-        }
-
        let newBook = new Book(title.value,author.value,pages.value,read);
        
         title.value = "";
@@ -68,6 +57,15 @@ bookForm.addEventListener("submit", (e) => {
 //Adds a new card with each book
 
 function addCard(title, author, pages, read) {
+
+    let book = myLibrary.find(book => book.title === title)
+
+    if (read === true) {
+        read = "read"
+    } else {
+        read = "not yet"
+    }
+    
     var element = document.createElement("div");
     element.className = "card";
     document.querySelector(".container").appendChild(element);
@@ -75,11 +73,60 @@ function addCard(title, author, pages, read) {
     <p class="title">${title}</p>
     <p class="author">${author}</p>
     <p class="pages">${pages}</p>
-    <p class="read">${read}</p>
-    <button class="readBtn">Read</button>
-    <button class="removeBook">Remove</button>
+    <p class="read" data-title="${book.title}">${read}</p>
+    <button class="readBtn" data-title="${book.title}">Read</button>
+    <button class="removeBook" data-title="${book.title}">Remove</button>
     `
-    
+
+    let rmvBtn = document.querySelectorAll(".removeBook");
+
+    for (let i = 0; i < rmvBtn.length; i++) {
+
+
+        if (!rmvBtn[i].classList.contains("listening")) {
+
+            rmvBtn[i].addEventListener("click", () => {
+                title = rmvBtn[i].getAttribute("data-title");
+                removeFromLibrary(title)
+                removeCard(title)
+            })
+
+            rmvBtn[i].classList.add("listening")
+        } 
+
+
+
+
+    }
+
+    let readBtn =document.querySelectorAll(".readBtn");
+
+    for (let i = 0; i < readBtn.length; i++) {
+        
+        if (!readBtn[i].classList.contains("listening")) {
+
+            readBtn[i].addEventListener("click", () => {
+
+                title = readBtn[i].getAttribute("data-title");
+                book = myLibrary.find(book => book.title === title)
+                console.log(book.read)
+                
+                if (book.read === false) {
+                    book.read = true
+                } else {
+                    book.read = false
+                }
+
+
+                
+                console.log(book.read)
+
+            })
+
+            readBtn[i].classList.add("listening")
+        } 
+
+    }
 }  
 
 //Book object contructor
@@ -90,30 +137,45 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    
 
     this.info = function() {
         return `${this.name} by ${this.author}, ${pages} pages, ${read}`;
     };
-
-    addCard(title, author, pages, read);
     addBookToLibrary(this);
-    console.log(myLibrary);
+    addCard(title, author, pages, read);
 
 }
 
 //Adds books to myLibrary array
 
 function addBookToLibrary(book) {
-    let arrNum = myLibrary.length;
-    book.num = arrNum;
     myLibrary.push(book);
     
 }
 
-//remove book from myLibrary
-
 
 //test book(s)
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
-const Mocking = new Book("To Kill a Mockingbird", "Harper Lee", 281, "not read yet")
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
+const Mocking = new Book("To Kill a Mockingbird", "Harper Lee", 281, true)
+
+//remove book from myLibrary
+
+function removeFromLibrary(title) {
+    book =  myLibrary.find(book => book.title === title);
+    index = myLibrary.indexOf(book);
+    console.log(index);
+    if (index > -1) {
+        myLibrary.splice(index, 1);
+    }    
+    console.log(myLibrary)
+}
+
+// remove card
+
+function removeCard(title) {
+    parent = document.querySelector(`[data-title="${title}"]`).parentNode;
+    console.log(parent);
+    parent.remove();
+}
